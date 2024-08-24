2ee39c6edd1a65c1a878ff28d9a41be8511ded6e
Mateus de Araújo Lima <mateuslima020@outlook.com>
2024-08-24 07:42:57 -0300

    feat: add History Page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
 
 export function Router() {
   return (
