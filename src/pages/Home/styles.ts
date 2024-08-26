a5f54c33237032d21078874c67d189d527ec7558
Mateus de Araújo Lima <mateuslima020@outlook.com>
2024-08-26 15:18:07 -0300

    feat: add active cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -47,6 +47,7 @@ const BaseInput = styled.input`
     color: ${(props) => props.theme['gray-500']};
   }
 `
+
 export const TaskInput = styled(BaseInput)`
   flex: 1;
 
@@ -59,7 +60,7 @@ export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
 `
 
-export const CountDownContainer = styled.div`
+export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
   line-height: 8rem;
@@ -74,17 +75,17 @@ export const CountDownContainer = styled.div`
     border-radius: 8px;
   }
 `
-
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme['green-500']};
+
   width: 4rem;
   overflow: hidden;
-
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -93,21 +94,32 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
 
   gap: 0.5rem;
   font-weight: bold;
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
